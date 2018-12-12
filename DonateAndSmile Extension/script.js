3688a7b578c34291f106ad9a8de9508b537c3fbf
Danny Guo <dannyguo91@gmail.com>
2018-12-12 15:09:35 -0500

    Implement the redirect
    The tricky cases are that some pages, like Amazon Music, don't support
    Smile, and other pages, like a product details page, won't load on
    the Smile domain if the user isn't logged in. For these cases, Amazon will
    redirect you back to the normal domain, leading to an infinite loop if the
    extension keeps trying to send the user to Smile.
    
    For the Chrome and Firefox extension, the WebExtensions API provides a
    requestId, which we can use to detect when we have already attempted a
    redirect. The Safari App Extension framework has no such notion, so the
    workaround is to set a "donateandsmile" query parameter when we try the
    redirect. If we get a request with the same query parameter, we know
    that Amazon has sent us back to the normal domain, and we should not
    attempt another redirect.
    
    We are counting on Amazon to preserve the query parameter through their
    redirect. From my testing, that seems to be a safe assumption for now.
    
    One alternative would be to maintain a blacklist of pages that don't
    support Smile, but that would require manually keeping the list up to
    date, and it also doesn't handle the pages where Smile is supported, but
    the user needs to log in.

--- DonateAndSmile Extension/script.js
+++ DonateAndSmile Extension/script.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    safari.extension.dispatchMessage("Hello World!");
-});
+(function () {
+    console.log(`Donate and Smile: checking ${window.location.href}`);
+    const url = new URL(window.location.href);
+
+    // AmazonSmile is only active in Germany, the U.S., and the U.K.
+    const matches = url.hostname.match(/^(?:www\.)?amazon\.(de|com|co\.uk)$/);
+    if (!matches) {
+        console.log('Donate and Smile: not redirecting because Smile is not supported for this region');
+        return;
+    }
+
+    // e.g. 'com', 'co.uk'
+    const extension = matches[1];
+
+    /* Prevent an infinite redirect loop if the particular page doesn't support
+       Smile or if the user isn't logged in. We are counting on Amazon to
+       preserve our query parameter through their redirect. */
+    const params = url.searchParams;
+    if (params.get('donateandsmile') === '1') {
+        console.log('Donate and Smile: not redirecting because Smile is not supported on this page');
+        return;
+    }
+    params.set('donateandsmile', '1');
+
+    const smileUrl = `https://smile.amazon.${extension}${url.pathname}?${params.toString()}${url.hash}`;
+    console.log(`Donate and Smile: redirecting to: ${smileUrl}`);
+    window.location.replace(smileUrl);
+})();
